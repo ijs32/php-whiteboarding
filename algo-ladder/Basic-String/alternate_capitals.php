5c29d1d6b493efb623aefcca3484bc51b9c3a0f1
ijs32 <iansilber32@gmail.com>
2022-04-13 20:11:08 -0400

    my first basic php algo

--- algo-ladder/Basic-String/alternate_capitals.php
+++ algo-ladder/Basic-String/alternate_capitals.php
@@ -1,4 +1,18 @@
-# Given a string, write a function that returns a copy of the original string that has every other character capitalized. (Capitalization should begin with the second character.)
+Given a string, write a function that returns a copy of the original string that has every other character capitalized. (Capitalization should begin with the second character.)
 
-# Input: “hello, how are your porcupines today?”
-# Output: “hElLo, HoW ArE YoUr pOrCuPiNeS ToDaY?”
+Input: “hello, how are your porcupines today?”
+Output: “hElLo, HoW ArE YoUr pOrCuPiNeS ToDaY?”
+
+<?php
+$string = 'hello, how are your porcupines today?';
+$i = 0;
+while ($i < strlen($string)) {
+    if ($i % 2 == 0) {
+        $string[$i] = strtoupper($string[$i]);
+    }
+    $i++;
+}
+echo $string;
+?>
+
+I am a php expert wow
